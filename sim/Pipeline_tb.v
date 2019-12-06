921beebfd9e4c93d582dd3e85ec545dd64f4f2a8
Will Lacey <WillLacey@Will-Laceys-MacBook-Pro-4.local>
2019-12-06 13:09:59 -0600

    changed test bench

--- sim/Pipeline_tb.v
+++ sim/Pipeline_tb.v
@@ -46,7 +46,7 @@ module Pipeline_tb;
 	initial
 	begin
 		clk = 1;
-		instruction32 = 32'hFFFFFFFF;
+		instruction32 = 32'h00000000;
 		regDst = 1;
 		regWrite = 1;
 	end
@@ -54,21 +54,14 @@ module Pipeline_tb;
 	// The following test bench code verifies the functionality of the Registers module
 	initial 
 	begin
-		#1 clk=0; instruction32=32'h00000000; regDst=0; regWrite=1;
-		#1 clk=1; instruction32=32'h11111111; regDst=0; regWrite=1;
-		#1 clk=0; instruction32=32'h22222222; regDst=0; regWrite=1;
-		#1 clk=1; instruction32=32'h33333333; regDst=0; regWrite=1;
-		#1 clk=0; instruction32=32'h44444444; regDst=0; regWrite=1;
-		#1 clk=1; instruction32=32'h55555555; regDst=0; regWrite=1;
-		#1 clk=0; instruction32=32'h66666666; regDst=0; regWrite=1;
-		#1 clk=1; instruction32=32'h77777777; regDst=0; regWrite=1;
-		#1 clk=0; instruction32=32'h88888888; regDst=0; regWrite=1;
-		#1 clk=1; instruction32=32'h99999999; regDst=0; regWrite=1;
-		#1 clk=0; instruction32=32'hAAAAAAAA; regDst=1; regWrite=1;
-		#1 clk=1; instruction32=32'hBBBBBBBB; regDst=0; regWrite=1;
+		#1 clk=0; instruction32=32'h00000000; regDst=1; regWrite=1;
+		#1 clk=1; instruction32=32'h00000001; regDst=1; regWrite=1;
+		#1 clk=0; instruction32=32'h00000002; regDst=1; regWrite=1;
+		#1 clk=1; instruction32=32'h00000003; regDst=1; regWrite=1;
+		#1 clk=0; instruction32=32'h00000004; regDst=1; regWrite=1;
 	end
 
-	initial $monitor($time, " Inputs: clk=%b instruction32=%h regDst=%b regWrite=%b \n\t\t\t Output: readData1=%h readData2=%h extImmediate=%h \n\t\t\t Decode: opcode=%b rs=%b rt=%b rd=%b shamt=%b funct=%b immediate=%b address=%b", clk, instruction32, regDst, regWrite, readData1, readData2, extImmediate, opcode, rs, rt, rd, shamt, funct, immediate, address);
+	initial $monitor($time, " Inputs: clk=%b instruction32=%h regDst=%b regWrite=%b \n\t\t\t Output: readData1=%h readData2=%h extImmediate=%h \n\t\t\t Decode: opcode=%b rs=%b rt=%b rd=%b shamt=%b funct=%b immediate=%b address=%b\n", clk, instruction32, regDst, regWrite, readData1, readData2, extImmediate, opcode, rs, rt, rd, shamt, funct, immediate, address);
 	initial #30 $stop;
 
 endmodule
